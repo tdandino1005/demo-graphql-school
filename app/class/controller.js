bba3f2ed66a28e28d7f18b54c65d38b0c60106a5
Manav Misra <manavm@visionwebsoft.com>
2023-03-09 17:01:45 -0600

    Add 'show' for class controller

--- app/class/controller.js
+++ app/class/controller.js
@@ -4,6 +4,9 @@ const controller = {
   index() {
     return ClassModel.find({}).populate("professor");
   },
+  show(id) {
+    return ClassModel.findById(id);
+  },
   updateClassBuilding(id, newBuilding) {
     return ClassModel.findByIdAndUpdate(id, { building: newBuilding });
   },
