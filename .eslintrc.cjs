7b2761b847df0a7d7dab4e4e35c91662a01a2041
Manav Misra <manavm@visionwebsoft.com>
2023-03-08 23:18:27 -0600

    Set up template repo starter codes 👨🏾‍💻

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,29 @@ module.exports = {
     node: true,
   },
   extends: ["standard", "prettier"],
-  overrides: [],
+  overrides: [
+    {
+      files: ["*.js"],
+      processor: "@graphql-eslint/graphql",
+    },
+    {
+      files: ["*.graphql"],
+      extends: [
+        "plugin:@graphql-eslint/schema-recommended",
+        "plugin:@graphql-eslint/operations-recommended",
+      ],
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {
-        "no-console":[
+    "no-console": [
       "warn",
       {
-               "allow": ["error", "info", "table", "warn"]
-      }
+        allow: ["error", "info", "table", "warn"],
+      },
     ],
   },
 };
