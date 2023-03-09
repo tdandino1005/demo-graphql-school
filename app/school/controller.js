018798ccca3869cc637ed4f8e67d4af2c45f13c7
Manav Misra <manavm@visionwebsoft.com>
2023-03-09 17:00:56 -0600

    Include 'create' for 🏫 controller

--- app/school/controller.js
+++ app/school/controller.js
@@ -1,6 +1,9 @@
 import SchoolModel from "./index.js";
 
 const controller = {
+  create(newSchool) {
+    return SchoolModel.create(newSchool);
+  },
   index() {
     return SchoolModel.find({}).populate({
       path: "classes",
