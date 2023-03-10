dd69dba37dbf5fd17985871c21bee1ccb3213082
Tony Dandino <tdandino1005@gmail.com>
2023-03-09 21:22:39 -0600

    updateds server.js

--- app/server.js
+++ app/server.js
@@ -1,27 +1,23 @@
-// TODO: https://www.apollographql.com/docs/apollo-server/api/express-middleware/
-
-// TODO: ⚠️ Wrap this in a function that we can call from `./index.js` to start the server
-import express from "express";
-import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-import { typeDefs, resolvers } from "./graphql/index.js";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+import { resolvers, typeDefs } from "./graphql/index.js";
 
 const app = express();
+
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-
-// Note you must call `server.start()` on the `ApolloServer`
-// instance before passing the instance to `expressMiddleware`
-await server.start();
-
-// Specify the path where we'd like to mount our server
-app.use("/", cors(), json(), express.json(), expressMiddleware(server));
 
 async function init() {
   // Note you must call `server.start()` on the `ApolloServer`
@@ -29,12 +25,12 @@ async function init() {
   await server.start();
 
   // Specify the path where we'd like to mount our server
-  app.use("/graphql", cors(), express.json(), expressMiddleware(server));
+  app.use("/", cors(), express.json(), expressMiddleware(server));
+
+  // Modified server startup
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+  console.info(`🚀 Server ready at http://localhost:4000/`);
 }
-
-// Modified server startup
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-
-console.info(`🚀 Server ready at http://localhost:4000/`);
 
 export default init;
