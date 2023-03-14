eaecca5b70c86d5f8bace505332c85a19fb0fbc1
Tony Dandino <tdandino1005@gmail.com>
2023-03-13 20:25:53 -0500

    updated  resolvers

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -2,19 +2,23 @@ import classController from "../class/controller.js";
 import professorController from "../professor/controller.js";
 import schoolController from "../school/controller.js";
 
+// We resolve the queries from 'typeDefs'.
+// The names must match.
 const resolvers = {
   Query: {
-    // We must not return a promise in a resolver
-    async indexClasses() {
+    async schools() {
+      return await schoolController.index();
+    },
+
+    async classes() {
       // Resolve the promise and return the result (not a promise)
       return await classController.index();
     },
-    async Professors() {
+
+    async professors() {
       return await professorController.index();
     },
-    async Schools() {
-      return await schoolController.index();
-    },
+
     async class(_, args) {
       return await classController.show(args._id);
     },
